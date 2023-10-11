3a5df6a5ed7517132f4238cf11d6ce8960d24427
MohamedAliAyoub <mohamedali163163@gmail.com>
2023-10-11 21:13:40 +0200

    2 IOC

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Coach thecoach = new Baseball();
+		Coach thecoach = new TrackCoach();
 		System.out.println( thecoach.getDailyWorkout());
 	}
 
---
+++ src/main/java/com/example/demo/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "make a new interface and test IDE ";
+    }
+}
